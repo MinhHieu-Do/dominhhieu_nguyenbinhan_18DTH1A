5db819b99160c26eec61a97810f088ac3234fcf3
MinhHieu_Do <dominhhieuntt@gmail.com>
2022-01-04 19:04:06 +0700

    initial commit

---
+++ src/app/Models/BenhNhan.ts
@@ -0,0 +1,17 @@
+import { User } from "./User";
+
+export class BenhNhan{
+
+         MaBN :any;
+         TenBN :any;
+         GioiTinh :any
+         NgaySinh :any
+         SDT :any
+         DiaChi :any
+         PassWord :any
+         UserType :any
+         LichKhams:any[]=[]
+         PhieuKhamBenhs:any[]=[]
+         HinhAnh :any
+
+}
